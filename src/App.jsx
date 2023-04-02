2b724fb04d67208afce16729ba182d2f1697b2be
Chae-Uk Lim <41909931+codnrawbwj@users.noreply.github.com>
2023-04-02 00:43:22 +0000

    Edited Header, Content, and Footer jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Header from './Header.jsx'
+import Content from './Content.jsx'
+import Footer from './Footer.jsx'
+
 
 export default function App() {
   return (
     <main>
-      Hello World!
+      <Header />
+      <Content />
+      <Footer />
     </main>
   )
 }
