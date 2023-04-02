4a2c4fdf8d75fd867dcac06a00d813da26ff9ab7
Chae-Uk Lim <41909931+codnrawbwj@users.noreply.github.com>
2023-04-02 01:03:49 +0000

    Added Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Header from './Header.jsx'
-import Content from './Content.jsx'
+import Employees from './Employees.jsx'
 import Footer from './Footer.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Content />
+      <Employees />
       <Footer />
     </main>
   )
